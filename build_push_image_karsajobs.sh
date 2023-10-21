e9e3e9fb91244b90e2197638daa4dddf424f449b
ThariqRamadhan101 <athariqramadhan@gmail.com>
2023-10-21 17:05:27 +0700

    fix typo

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -2,7 +2,7 @@
 
 # Perintah untuk build Docker image dari berkas Dockerfile yang disediakan
 docker build -t ghcr.io/thariqramadhan101/karsajobs:latest .
-# Perintah untuk login ke GitHub Package
+# Perintah untuk login ke GitHub Packages
 docker login ghcr.io -u thariqramadhan101 -p $GH_PACKAGES_TOKEN
 # Perintah untuk push image ke GitHub Packages
 docker push ghcr.io/thariqramadhan101/karsajobs:latest
