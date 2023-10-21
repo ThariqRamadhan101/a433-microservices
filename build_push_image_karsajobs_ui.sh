5f3ac2bd92207c4b752dec544a85f97e11aca969
ThariqRamadhan101 <athariqramadhan@gmail.com>
2023-10-21 17:04:49 +0700

    add build push image sh for karsajob-ui

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Perintah untuk build Docker image dari berkas Dockerfile yang disediakan
+docker build -t ghcr.io/thariqramadhan101/karsajobs-ui:latest .
+# Perintah untuk login ke GitHub Packages
+docker login ghcr.io -u thariqramadhan101 -p $GH_PACKAGES_TOKEN
+# Perintah untuk push image ke GitHub Packages
+docker push ghcr.io/thariqramadhan101/karsajobs-ui:latest
